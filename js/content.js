c53624f12b62143aecfedac1d3b0d0868a258261
Charity Nyanchera <charitynyanchera@gmail.com>
2022-03-19 18:10:13 +0300

    st:nav --position-fixed

--- js/content.js
+++ js/content.js
@@ -1,9 +1,9 @@
 $(".show-menu").click(()=>{
-    $("nav").toggleClass("show-nav");
-    $(".show-menu").toggle();
+    $("nav").toggle();
+        $(".show-menu").toggle();
 })
 
 $(".tog-nav").click(()=>{
-     $("nav").toggleClass("show-nav");
+     $("nav").toggle();
     $(".show-menu").toggle();
 })
